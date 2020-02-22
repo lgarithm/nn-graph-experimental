cd704ebeaaff674fa979f637e51b04ed78cda2e4
lg <lg4869@outlook.com>
2020-02-22 22:30:54 +0000

    update deps

--- include/nn/bits/graph/trace.hpp
+++ include/nn/bits/graph/trace.hpp
@@ -2,9 +2,5 @@
 #ifdef NN_GRAPH_ENABLE_TRACE
 #    include <tracer/simple>
 #else
-#    define TRACE_CUDA_SCOPE(name)
-#    define TRACE_SCOPE(name)
-#    define LOG_SCOPE(name)
-#    define TRACE_STMT(e) e;
-#    define TRACE_EXPR(e) e
+#    include <tracer/disable>
 #endif
