073365d31acfaf4b23caabfd6cddddc9499fad2f
lg <lg4869@outlook.com>
2020-04-03 21:12:05 +0100

    update URL

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,20 +1,24 @@
 SET(STDTRACER_GIT_URL
-    https://github.com/lgarithm/stdtracer.git
+    https://github.com/stdml/stdtracer.git
     CACHE STRING "URL for clone stdtracer")
 
 # stdtensor
 SET(STDTENSOR_GIT_URL
-    https://github.com/lgarithm/stdtensor.git
+    https://github.com/stdml/stdtensor.git
     CACHE STRING "URL for clone stdtensor")
 
-SET(STDTENSOR_GIT_TAG "master" CACHE STRING "git tag for checkout stdtensor")
+SET(STDTENSOR_GIT_TAG
+    "master"
+    CACHE STRING "git tag for checkout stdtensor")
 
 # stdnn-ops
 SET(STDNN_OPS_GIT_URL
-    https://github.com/lgarithm/stdnn-ops.git
+    https://github.com/stdml/stdnn-ops.git
     CACHE STRING "URL for clone stdnn-ops")
 
-SET(STDNN_OPS_GIT_TAG "master" CACHE STRING "git tag for checkout stdnn-ops")
+SET(STDNN_OPS_GIT_TAG
+    "master"
+    CACHE STRING "git tag for checkout stdnn-ops")
 
 # stdnn-ops-cuda
 SET(STDNN_OPS_CUDA_GIT_URL
